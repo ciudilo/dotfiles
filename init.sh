2c0a5662ee00aa93973e9c0aa6717c28cb3238a8
Kiril Piskunov <kpiskunov@itrsgroup.com>
2014-03-13 10:44:14 +0000

    sublime settings

--- init.sh
+++ init.sh
@@ -3,3 +3,10 @@
 cd
 ln -s dotfiles/.tmux.conf .tmux.conf
 ln -s dotfiles/.zshrc .zshrc
+
+cd - &> /dev/null
+# Install Sublime Text settings
+if [ ~/Library/Application\ Support/Sublime\ Text\ 3/ -e ]; then
+	mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings.bac 2> /dev/null
+	cp init/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings 2> /dev/null
+fi
