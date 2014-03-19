1e114841831727c72bcba2d786af0398e3031e2f
Kiril Piskunov <ciudilo@gmail.com>
2014-03-19 14:18:52 +0000

    init script was not working on linux

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ ln -s dotfiles/.zshrc .zshrc
 
 cd - &> /dev/null
 # Install Sublime Text settings
-if [ ~/Library/Application\ Support/Sublime\ Text\ 3/ -e ]; then
+if [[ -e "~/Library/Application\ Support/Sublime\ Text\ 3/" ]]; then
 	mv ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings.bac 2> /dev/null
 	cp init/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings 2> /dev/null
 fi
